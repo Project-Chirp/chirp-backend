26fe9383d5dcdebcc70ca564cd05ee0486748804
Dennull <lieudennis7@gmail.com>
2023-02-28 21:12:24 -0700

    Got basic auth working for api

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const express = require("express");
-const { expressjwt: jwt } = require("express-jwt");
-const jwks = require("jwks-rsa");
+// Modules
 const axios = require("axios");
 const appUserRoute = require("./Routes/appUsersRoutes");
 
+// Middlewears
 const cors = require("cors");
-const { response } = require("express");
-const app = express();
-const port = process.env.SERVER_PORT || 3001;
-require("dotenv").config();
-
-app.use(express.json());
-app.use(cors());
+const express = require("express");
+const { expressjwt: jwt } = require("express-jwt");
+const jwks = require("jwks-rsa");
 
 const verifyJwt = jwt({
   secret: jwks.expressJwtSecret({
@@ -25,19 +20,33 @@ const verifyJwt = jwt({
   algorithms: ["RS256"],
 }).unless({ path: ["/"] });
 
-app.use(verifyJwt);
+const app = express();
+const port = process.env.SERVER_PORT || 3001;
+require("dotenv").config();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(cors());
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Internal server error";
+  res.status(status).send(message);
+});
+
+app.use("/api/appUsers", appUserRoute);
+
+app.get("/public", (req, res) => {
+  console.log(req);
   res.send("Hello World!");
 });
 
-app.get("/protected", async (req, res) => {
+app.get("/protected", verifyJwt, async (req, res) => {
   try {
-    const accessToken = req.headers.authorization.split("")[1];
+    const accessToken = req.headers.authorization.split(" ")[1];
     const response = await axios.get(
-      "https://dev-8e2eney2zngtvaof.us.auth0.com/userinfo",
+      "https://dev-gxkwzphy3vwb5jqh.us.auth0.com/userinfo",
       {
-        header: {
+        headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       }
@@ -48,87 +57,6 @@ app.get("/protected", async (req, res) => {
   } catch (error) {
     res.send(error.message);
   }
-
-  // res.send("Hello from protected route!");
 });
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message || "Internal server error";
-  res.status(status).send(message);
-});
-
-app.use("/api/appUsers", appUserRoute);
 app.listen(port, () => console.log(`Listening on port ${port}....`));
-
-// let str = {
-//   content: "Hello World!",
-//   subContent: "HELLO",
-// };
-// const testRoute = require("./testRoute");
-// const { Client } = require("pg");
-// const client = new Client({
-//   host: "localhost",
-//   user: "postgres",
-//   port: "5432",
-//   password: "twitter",
-//   database: "twitter_clone",
-// });
-
-// client.connect();
-
-// client.query(`Select * from app_user`, (err, res) => {
-//   if (!err) {
-//     console.log(res.rows);
-//   } else {
-//     console.log(err.message);
-//   }
-//   client.end;
-// });
-
-// const users = [
-//   { id: 1, name: "Messi" },
-//   { id: 2, name: "Ronaldo" },
-//   { id: 3, name: "Mbappe" },
-// ];
-
-// app.get("/hello", (req, res) => {
-//   res.send("Hello World!!");
-// });
-
-// app.get("/api/users", (req, res) => {
-//   res.send(users);
-// });
-
-// app.get("/api/usersTweet", (req, res) => {
-//   client.query(`Select * from app_user`, (err, res) => {
-//     if (!err) {
-//       console.log(res.rows);
-//     } else {
-//       console.log(err.message);
-//     }
-//     client.end;
-//   });
-// });
-
-// app.get("/api/users/:id", (req, res) => {
-//   let user = users.find((c) => c.id === parseInt(req.params.id));
-//   if (!user) {
-//     res.status(404).send("User with that ID does not exist.");
-//   }
-//   res.send(user);
-// });
-
-// app.get("/api/users/:id/:name", (req, res) => {
-//   res.send(req.params);
-// });
-
-// app.get('/api/user/:pid/:Fname', (req, res)=> {
-//     res.send(req.query);
-// });
