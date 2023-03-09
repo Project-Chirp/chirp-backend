a0fb59839f27fd84fba39a79c0a86bf9bcc07b45
Dennull <lieudennis7@gmail.com>
2023-03-08 20:14:03 -0700

    Some cleanup

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// Modules
+// Imports
 const axios = require("axios");
-const appUserRoute = require("./Routes/appUsersRoutes");
-const appPostRoute = require("./routes/postRoutes");
-const pool = require("./database/db");
-
-// Middlewares
 const cors = require("cors");
 const express = require("express");
 const { expressjwt: jwt } = require("express-jwt");
 const jwks = require("jwks-rsa");
+
+const pool = require("./database/db");
+const appUserRoute = require("./Routes/appUsersRoutes");
+const appPostRoute = require("./routes/postRoutes");
 
 const verifyJwt = jwt({
   secret: jwks.expressJwtSecret({
@@ -24,13 +23,11 @@ const verifyJwt = jwt({
 
 const currentUserCheck = async (req, res, next) => {
   const auth0Id = req.auth.sub;
-  // console.log(auth0Id);
   const query = await pool.query(
     `SELECT * from app_user WHERE "auth0Id" = $1`,
     [auth0Id]
   );
   const user = query.rows;
-  console.log(user);
   if (!user) {
     try {
       const accessToken = req.headers.authorization.split(" ")[1];
@@ -43,13 +40,12 @@ const currentUserCheck = async (req, res, next) => {
         }
       );
       const userInfo = response.data;
-      // console.log(userInfo);
       pool.query(
         `INSERT INTO app_user ("auth0Id", email, "joinedDate") VALUES ($1, $2, $3)`,
         [userInfo.sub, userInfo.email, new Date()]
       );
     } catch (error) {
-      res.send(error.message);
+      console.log(error);
     }
   }
   next();
@@ -62,12 +58,6 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-// app.use((error, req, res, next) => {
-//   const status = error.status || 500;
-//   const message = error.message || "Internal server error";
-//   res.status(status).send(message);
-// });
-
 app.use("/api/appUsers", appUserRoute);
 app.use("/api/posts", appPostRoute);
 
@@ -76,58 +66,7 @@ app.get("/public", (req, res) => {
 });
 
 app.get("/protected", verifyJwt, currentUserCheck, async (req, res) => {
-  try {
-    const accessToken = req.headers.authorization.split(" ")[1];
-    const response = await axios.get(
-      "https://dev-gxkwzphy3vwb5jqh.us.auth0.com/userinfo",
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    const userInfo = response.data;
-    // console.log(userInfo);
-    console.log(accessToken);
-    res.send(userInfo);
-  } catch (error) {
-    res.send(error.message);
-  }
+  res.send("Hello from protected route!");
 });
-
-// app.get(
-//   "/protected",
-//   jwt({
-//     secret: jwks.expressJwtSecret({
-//       cache: true,
-//       rateLimit: true,
-//       jwksRequestsPerMinute: 5,
-//       jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-//     }),
-//     audience: process.env.AUTH0_AUDIENCE,
-//     issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-//     algorithms: ["RS256"],
-//   }),
-//   async (req, res) => {
-//     try {
-//       const accessToken = req.headers.authorization.split(" ")[1];
-//       const response = await axios.get(
-//         "https://dev-gxkwzphy3vwb5jqh.us.auth0.com/userinfo",
-//         {
-//           headers: {
-//             Authorization: `Bearer ${accessToken}`,
-//           },
-//         }
-//       );
-//       const userInfo = response.data;
-//       // console.log(userInfo);
-//       // console.log(accessToken);
-//       console.log(req.auth);
-//       res.send(userInfo);
-//     } catch (error) {
-//       res.send(error.message);
-//     }
-//   }
-// );
 
 app.listen(port, () => console.log(`Listening on port ${port}....`));
