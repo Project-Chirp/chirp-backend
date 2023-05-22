10743bd554f36b1e32115ad7ad22d3a970769a28
Dennull <lieudennis7@gmail.com>
2023-05-22 16:22:58 -0600

    Rename message timestamp database column

--- models/MessagesModel.js
+++ models/MessagesModel.js
@@ -1,14 +1,14 @@
 const getLatestMessages = `SELECT DISTINCT ON(otherUser) "displayName",
   username,
   "textContent",
-  "messageTimestamp"
+  timestamp
   FROM 
     (SELECT m.*, 
      CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUser 
     FROM message m
     WHERE m."sentUserId" = $1 OR m."receivedUserId" = $1) s
   JOIN app_user u ON s.otherUser = u."userId"
-  ORDER BY otherUser, "messageTimestamp" DESC;`;
+  ORDER BY otherUser, timestamp DESC;`;
 
 module.exports = {
   getLatestMessages,
