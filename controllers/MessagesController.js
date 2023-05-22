86853b5f16a539c028a19eeccbc4084095158d2f
Dennull <lieudennis7@gmail.com>
2023-05-22 16:17:26 -0600

    Make latest message query specific to user

--- controllers/MessagesController.js
+++ controllers/MessagesController.js
@@ -3,8 +3,8 @@ const messageQueries = require("../models/MessagesModel");
 
 const getLatestMessages = async (req, res) => {
   try {
-    const userId = req.query;
-    const query = await pool.query(messageQueries.getLatestMessages);
+    const { userId } = req.query;
+    const query = await pool.query(messageQueries.getLatestMessages, [userId]);
     res.send(query.rows);
   } catch (error) {
     console.log(error);
--- models/MessagesModel.js
+++ models/MessagesModel.js
@@ -1,13 +1,12 @@
-// Define the query to get the latest message between 2 users to display as a list
 const getLatestMessages = `SELECT DISTINCT ON(otherUser) "displayName",
   username,
   "textContent",
   "messageTimestamp"
   FROM 
     (SELECT m.*, 
-     CASE WHEN "sentUserId" = 1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUser 
+     CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUser 
     FROM message m
-    WHERE m."sentUserId" = 1 OR m."receivedUserId" = 1) s
+    WHERE m."sentUserId" = $1 OR m."receivedUserId" = $1) s
   JOIN app_user u ON s.otherUser = u."userId"
   ORDER BY otherUser, "messageTimestamp" DESC;`;
 
