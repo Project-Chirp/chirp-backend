2b8b5cfff63f602b15801242b1722b87bd974138
Dennull <lieudennis7@gmail.com>
2023-05-30 22:44:04 -0600

    Formatting

--- models/MessagesModel.js
+++ models/MessagesModel.js
@@ -1,14 +1,15 @@
-const getLatestMessages = `SELECT DISTINCT ON(otherUser) "displayName",
+const getLatestMessages = `SELECT DISTINCT ON(otherUserId)
+  "displayName",
   username,
   "textContent",
   timestamp
-  FROM 
-    (SELECT m.*, 
-     CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUser 
+  FROM
+    (SELECT m.*,
+     CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUserId
     FROM message m
     WHERE m."sentUserId" = $1 OR m."receivedUserId" = $1) s
-  JOIN app_user u ON s.otherUser = u."userId"
-  ORDER BY otherUser, timestamp DESC;`;
+  JOIN app_user u ON s.otherUserId = u."userId"
+  ORDER BY otherUserId, timestamp DESC;`;
 
 module.exports = {
   getLatestMessages,
