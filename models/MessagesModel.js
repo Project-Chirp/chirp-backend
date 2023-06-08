d467ae357e43dca08328ac88a9c1fb743b601643
NasihNazeem <nasihnazeem@gmail.com>
2023-06-08 17:15:00 -0600

    updated queries to return otherUserId instead of otheruserid or userid"

--- models/MessagesModel.js
+++ models/MessagesModel.js
@@ -11,18 +11,19 @@ const getLatestMessages = `SELECT DISTINCT ON(otherUserId)
   JOIN app_user u ON s.otherUserId = u."userId"
   ORDER BY otherUserId, timestamp DESC;`;
 
-const getDMList = `SELECT DISTINCT ON(otherUserId)
+const getDMList = `SELECT DISTINCT ON("otherUserId")
+"otherUserId",
 "displayName",
 username
 FROM
   (SELECT m.*,
-   CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUserId
+   CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS "otherUserId"
   FROM message m
   WHERE m."sentUserId" = $1 OR m."receivedUserId" = $1) s
-JOIN app_user u ON s.otherUserId = u."userId"
-ORDER BY otherUserId, timestamp DESC;`;
+JOIN app_user u ON s."otherUserId" = u."userId"
+ORDER BY "otherUserId", timestamp DESC;`;
 
-const getFollowedList = `SELECT u."userId", u."displayName", u."username"
+const getFollowedList = `SELECT u."userId" AS "otherUserId", u."displayName", u."username"
 FROM app_user u
 JOIN follow f ON u."userId" = f."followedUserId"
 WHERE f."followerUserId" = $1 AND f."followStatus" = true;`;
