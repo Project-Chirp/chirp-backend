5f0cd40b40cfc326431a1f90d95e42f7d985867a
Dennull <lieudennis7@gmail.com>
2023-06-09 00:20:50 -0600

    Direct message endpoint

--- controllers/MessagesController.js
+++ controllers/MessagesController.js
@@ -12,6 +12,23 @@ const getLatestMessages = async (req, res) => {
   }
 };
 
+const getDirectMessage = async (req, res) => {
+  try {
+    console.log(req.params);
+    const { userId1, userId2 } = req.params;
+    console.log(userId1, userId2);
+    const query = await pool.query(messageQueries.getDirectMessage, [
+      userId1,
+      userId2,
+    ]);
+    res.send(query.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
 module.exports = {
+  getDirectMessage,
   getLatestMessages,
 };
--- models/MessagesModel.js
+++ models/MessagesModel.js
@@ -1,16 +1,22 @@
-const getLatestMessages = `SELECT DISTINCT ON(otherUserId)
+const getDirectMessage = `SELECT * FROM message
+  WHERE "sentUserId" in ($1, $2) AND "receivedUserId" in ($1, $2)
+  ORDER BY timestamp`;
+
+const getLatestMessages = `SELECT DISTINCT ON("otherUserId")
   "displayName",
   username,
   "textContent",
-  timestamp
+  timestamp,
+  s."otherUserId"
   FROM
     (SELECT m.*,
-     CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS otherUserId
+     CASE WHEN "sentUserId" = $1 THEN "receivedUserId" ELSE "sentUserId" END AS "otherUserId"
     FROM message m
     WHERE m."sentUserId" = $1 OR m."receivedUserId" = $1) s
-  JOIN app_user u ON s.otherUserId = u."userId"
-  ORDER BY otherUserId, timestamp DESC;`;
+  JOIN app_user u ON s."otherUserId" = u."userId"
+  ORDER BY "otherUserId", timestamp DESC;`;
 
 module.exports = {
+  getDirectMessage,
   getLatestMessages,
 };
