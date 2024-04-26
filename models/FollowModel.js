6ba93463a50e4388ff3112b4c48b84ce6828c80c
NasihNazeem <nasihnazeem@gmail.com>
2024-04-25 22:14:59 -0600

    simplified follow and getfollowStatus queries

--- models/FollowModel.js
+++ models/FollowModel.js
@@ -1,7 +1,5 @@
 const followUser = `INSERT INTO follow ("followerUserId", "followedUserId")
-SELECT $1, "userId"
-FROM app_user
-WHERE "userId" = $2;
+VALUES ($1, $2)
 `;
 
 const unfollowUser = `DELETE FROM follow
@@ -10,11 +8,10 @@ AND "followedUserId" = $2;
 `;
 
 const getFollowStatus = `SELECT EXISTS (
-  SELECT 1 
-  FROM follow 
-  WHERE "followerUserId" = $1 
-  AND "followedUserId" = (SELECT "userId" FROM app_user WHERE "userId" = $2)
-) AS "followStatus";
+  SELECT 1
+  FROM follow
+  WHERE "followerUserId" = $1 AND "followedUserId" = $2
+) as "followStatus";
 `;
 
 module.exports = {
