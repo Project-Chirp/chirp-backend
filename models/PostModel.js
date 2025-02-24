e98e4a9a258797ace84c4fd27d871ab64b0b467e
NasihNazeem <nasihnazeem@gmail.com>
2025-02-23 18:04:35 -0700

    update isRepostedByCurrentUser condition in getPost

--- models/PostModel.js
+++ models/PostModel.js
@@ -167,9 +167,10 @@ const getPost = `
     EXISTS (
       SELECT 1 
       FROM post p2
-      WHERE p2."repostedBy" = $1 
+      WHERE p2."repostedBy" = $1
         AND p2."textContent" IS NULL
-		    AND p2."postId" = p."postId" 
+		    AND (p2."parentPostId" = p."postId")
+        AND p2."deleted" = FALSE
       LIMIT 1
     ) AS "isRepostedByCurrentUser",
      CASE
